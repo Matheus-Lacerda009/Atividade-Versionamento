98be88a83fd6a1bb7a00b8acf4d0b7759ec6d03f
matheus-lacerda009 <matheuslacerda.mala@gmail.com>
2026-05-13 19:08:35 -0300

    Adicionando funções e validando operação

--- Base.py
+++ Base.py
@@ -1,14 +1,27 @@
-op = input("Insira a operação desejada:\n")
 num1 = float(input("Insira o primeiro número da operação:\n"))
 num2 = float(input("Insira o segundo número da operação:\n"))
-match op:
-    case "+":
-        print("Resultado:", num1 + num2)
-    case "-":
-        print("Resultado:", num1 - num2)
-    case "/":
-        print("Resultado:", num1 / num2)
-    case "*":
-        print("Resultado:", num1 * num2)
-    case _:
-        print("Operação não encontrada!")
+op = ""
+def soma(a, b):
+    return a + b
+def subtracao(a, b):
+    return a - b
+def divisao(a, b):
+    return a / b
+def multiplicacao(a, b):
+    return a * b
+while op not in ["+", "-", "/", "*"]:
+    op = input("Insira a operação desejada:\nSoma: +\nSubtração: -\nDivisão: /\nMultiplicação: *\n")
+    match op:
+        case "+":
+            print("Resultado:", soma(num1, num2))
+            break
+        case "-":
+            print("Resultado:", subtracao(num1, num2))
+            break
+        case "/":
+            print("Resultado:", divisao(num1, num2))
+            break
+        case "*":
+            print("Resultado:", multiplicacao(num1, num2))
+            break
+    print("Operação não encontrada!")
