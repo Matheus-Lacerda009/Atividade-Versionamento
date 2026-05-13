ade5a8653409149be3107256debfa883838a0ace
matheus-lacerda009 <matheuslacerda.mala@gmail.com>
2026-05-13 18:50:47 -0300

    Alterando tipos de variáveis e arrumando outputs com os resultados

--- .history/Base_20260513184955.py
+++ .history/Base_20260513184955.py
@@ -1,14 +1,14 @@
 op = input("Insira a operação desejada:\n")
-num1 = float(input("Insira o primeiro número da operação:\n"))
-num2 = float(input("Insira o segundo número da operação:\n"))
+num1 = int(input("Insira o primeiro número da operação:\n"))
+num2 = int(input("Insira o segundo número da operação:\n"))
 match op:
     case "+":
         print("Resultado:", num1 + num2)
     case "-":
         print("Resultado:", num1 - num2)
     case "/":
-        print("Resultado:", num1 / num2)
+        print("Resultado: ", num1 / num2)
     case "*":
-        print("Resultado:", num1 * num2)
+        print("Resultado: ", num1 * num2)
     case _:
         print("Operação não encontrada!")
--- .history/Base_20260513184956.py
+++ .history/Base_20260513184956.py
@@ -1,6 +1,6 @@
 op = input("Insira a operação desejada:\n")
-num1 = float(input("Insira o primeiro número da operação:\n"))
-num2 = float(input("Insira o segundo número da operação:\n"))
+num1 = int(input("Insira o primeiro número da operação:\n"))
+num2 = int(input("Insira o segundo número da operação:\n"))
 match op:
     case "+":
         print("Resultado:", num1 + num2)
