d50a241829edd96c9a70d74f1318d22c5ef62410
matheus-lacerda009 <matheuslacerda.mala@gmail.com>
2026-05-20 19:42:01 -0300

    Adicionando exponencial e porcentagem

--- .history/Base_20260520194138.py
+++ .history/Base_20260520194138.py
@@ -10,15 +10,11 @@ def divisao(a, b):
     return a / b
 def multiplicacao(a, b):
     return a * b
-def exponencial(a, b):
-    return a ** b
-def porcentagem(a, b):
-    return a * (b / 100)
 while sair == False:
     num1 = float(input("Insira o primeiro número da operação:\n"))
     num2 = float(input("Insira o segundo número da operação:\n"))
     while op not in ["+", "-", "/", "*"]:
-        op = input("Insira a operação desejada:\nSoma: +\nSubtração: -\nDivisão: /\nMultiplicação: *\nExponencial: ^\nPorcentagem: %\n")
+        op = input("Insira a operação desejada:\nSoma: +\nSubtração: -\nDivisão: /\nMultiplicação: *\n")
         match op:
             case "+":
                 print("Resultado:", soma(num1, num2))
@@ -31,12 +27,6 @@ while sair == False:
                 break
             case "*":
                 print("Resultado:", multiplicacao(num1, num2))
-                break
-            case "^":
-                print("Resultado:", exponencial(num1, num2))
-                break
-            case "%":
-                print("Resultado:", porcentagem(num1, num2))
                 break
         print("Operação não encontrada!")
     sair = bool(input("Digite 0 para sair: "))
